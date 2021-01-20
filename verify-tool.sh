5a64c2760c029e427f44b56300265356e01b6c69
Log_N <Log-N@outlook.com>
2021-01-20 03:48:53 -0330

    Add files via upload

---
+++ verify-tool.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env bash
+
+
+echo "
+=============================================== 
+"
+
+date
+
+cat /etc/os-release
+
+echo "
+=============================================== 
+"
+
+HOSTNAME=`uname -n`
+ROOT=`df -h`
+
+MEMORY1=`free -t -m | grep Total | awk '{print $3" MB";}'`
+MEMORY2=`free -t -m | grep "Mem" | awk '{print $2" MB";}'`
+LOAD1=`cat /proc/loadavg | awk {'print $1'}`
+LOAD5=`cat /proc/loadavg | awk {'print $2'}`
+LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+
+echo "
+===============================================
+ - Hostname............: $HOSTNAME
+ - Disk Space..........: $ROOT
+===============================================
+ - CPU usage...........: $LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)
+ - Memory used.........: $MEMORY1 / $MEMORY2
+ - Swap in use.........: `free -m | tail -n 1 | awk '{print $3}'` MB
+===============================================
+"
